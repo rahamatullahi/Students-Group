a0f01ac4fcf6af690c2e9e4f5e2b0e4534d71970
rahamatullahi <osmanrahamatullahi@gmail.com>
2024-11-07 01:21:54 -1200

    Add files via upload

---
+++ home.js
@@ -0,0 +1,43 @@
+
+const loginForm = document.getElementById('login-form');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const loginBtn = document.getElementById('login-btn');
+const showPasswordCheckbox = document.getElementById('rememberme1');
+const message = document.getElementById('message');
+const users = [
+  { username: 'john', password: 'johnsPassword' },
+  { username: 'jane', password: 'janesPassword' },
+  { username: 'bob', password: 'bobsPassword' },
+];
+
+loginBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  const user = users.find((user) => user.username === username);
+
+  
+if (user && user.password === password) {
+  message.style.color = 'green';
+  message.textContent = 'Login successful! Welcome, ' + username + '!';
+
+  // Wait for 3 seconds before redirecting
+  setTimeout(() => {
+    window.location.href = '3d nav.html'; // Redirect to treasure chest page!
+  }, 3000); // 3000 milliseconds = 3 seconds
+} else {
+  message.style.color = 'red';
+  message.textContent = 'Invalid username or password.';
+}
+
+});
+showPasswordCheckbox.addEventListener('change', (e) => {
+  if (showPasswordCheckbox.checked) {
+    passwordInput.type = 'text';
+  } else {
+    passwordInput.type = 'password';
+  }
+});
